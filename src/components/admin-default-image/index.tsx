87976f3cdf69050d0d8248e833777440baef4203
Avinashpotnuru <potnuruaavinash111@example.com>
2025-07-02 16:16:37 +0530

    add class to admin image

--- src/components/admin-default-image/index.tsx
+++ src/components/admin-default-image/index.tsx
@@ -1,6 +1,16 @@
 import AdminImage from '/admin.png';
-const AdminDefaultImage = ({ height = 70, width = 70, src }: { height?: number; width?: number; src?: string }) => {
-	return <img height={height} width={width} src={src || AdminImage} alt="Admin" />;
+const AdminDefaultImage = ({
+	height = 70,
+	width = 70,
+	src,
+	className,
+}: {
+	height?: number;
+	width?: number;
+	src?: string;
+	className?: string;
+}) => {
+	return <img height={height} width={width} src={src || AdminImage} className={className} alt="Admin" />;
 };
 
 export default AdminDefaultImage;
