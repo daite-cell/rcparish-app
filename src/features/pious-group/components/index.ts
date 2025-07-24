0d677c3113ca505cfe0fe8323a3f494fa3f21f89
Avinashpotnuru <potnuruaavinash111@example.com>
2025-07-24 18:31:49 +0530

    added forms in internal orders

--- src/features/pious-group/components/index.ts
+++ src/features/pious-group/components/index.ts
@@ -2,3 +2,4 @@ export { default as RenderPiousGroupOverviewContainer } from './render-pious-gro
 export { default as RenderPiousGroupTables } from './render-pious-group-tables';
 export { default as CouncilMemberDetailsContainer } from './generic-religious-people-details-container';
 export { default as GenericMembersInFamilesOverviewContainer } from './generic-members-in-familes-overview-container';
+export { default as FormsContainer } from './forms-container';
--- src/features/pious-group/pages/pious-group-generic-page/index.tsx
+++ src/features/pious-group/pages/pious-group-generic-page/index.tsx
@@ -3,20 +3,29 @@ import { side_nav_links } from '@/data/side-navbar-content';
 import type { NavLinkProps } from '@/types';
 import { getSectionByPathName } from '@/utils/getSectionByPathName';
 import { useRouteName } from '@/utils/getRouteName';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { useStore } from '@/store/store';
-import { RenderPiousGroupOverviewContainer, RenderPiousGroupTables } from '../../components';
+import { FormsContainer, RenderPiousGroupOverviewContainer, RenderPiousGroupTables } from '../../components';
+import { usePathName } from '@/utils/getPathName';
 
 const PiousGroupGenericPage = () => {
-	const [activeIndex, setActiveIndex] = useState(1);
 	const type = useRouteName('type');
+	const pathName = usePathName();
+
 	const { selectRow, selectFamilyCardRow, selectPriorRow } = useStore();
 	const handleToggleTab = (index: number) => {
 		setActiveIndex(index);
 	};
 
-	const linksData = getSectionByPathName(side_nav_links, type as string);
-	const tabsData = linksData?.page_nav_links.find((link: NavLinkProps) => link.path_url === type)?.tabs;
+	const linksData = getSectionByPathName(side_nav_links, pathName);
+	const tabsData = linksData?.page_nav_links.find((link: NavLinkProps) => link.path_url === pathName)?.tabs;
+	const defaultTabIndex = useMemo(() => {
+		if (!tabsData) return 0;
+		const viewIndex = tabsData.findIndex((tab) => tab.label.toLowerCase() === 'view');
+		return viewIndex !== -1 ? viewIndex : 0;
+	}, [tabsData]);
+
+	const [activeIndex, setActiveIndex] = useState(defaultTabIndex);
 
 	if (selectPriorRow) {
 		return <PriorDignitariesContainer />;
@@ -27,12 +36,14 @@ const PiousGroupGenericPage = () => {
 	}
 
 	return (
-		<TabsLayout
-			tabs={tabsData || [{ label: 'view' }, { label: 'add' }]}
-			onTabChange={handleToggleTab}
-			activeTabId={activeIndex}
-		>
-			{activeIndex === 1 && <RenderPiousGroupTables />}
+		<TabsLayout tabs={tabsData || []} onTabChange={handleToggleTab} activeTabId={activeIndex}>
+			{tabsData?.[activeIndex]?.label === 'view' ? (
+				<RenderPiousGroupTables />
+			) : tabsData?.[activeIndex]?.label.toLowerCase() === 'add' ? (
+				<FormsContainer />
+			) : (
+				<h1>charge</h1>
+			)}
 		</TabsLayout>
 	);
 };
